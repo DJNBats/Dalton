b37f753072e022b7e52cd4cceb10fb3b5a64d6e1
Dalton Baptista <daltonbaptista5@gmail.com>
2025-02-04 10:01:54 -0100

    correcao no footer

--- 01 - Formulário de envia dados/envia.php
+++ 01 - Formulário de envia dados/envia.php
@@ -4,12 +4,12 @@
     $email = addslashes($_POST['email']);
     $telefone = addslashes($_POST['telefone']);
 
-    $para = "intelioteste@gmail.com";
-    $assunto = "Coleta de dados - Inteliogia";
+    $para = "catnee@org.cv";
+    $assunto = "Coleta de dados - Catnee";
 
     $corpo = "Nome: ".$nome."\n"."E-mail: ".$email."\n"."Telefone: ".$telefone;
 
-    $cabeca = "From: teste@inteliogia.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
+    $cabeca = "From: teste@catnee.cv"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
 
     if(mail($para,$assunto,$corpo,$cabeca)){
         echo("E-mail enviado com sucesso!");
